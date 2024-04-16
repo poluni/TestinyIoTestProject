2029df3be61844d28cafd3bcd952f12239b5af5f
Olga P. <olya-polunina@yandex.ru>
2024-04-16 21:05:20 +0300

    Added DashboardPage

---
+++ TestinyTestSolution/TestinyTestProject/Pages/DashboardPage.cs
@@ -0,0 +1,35 @@
+﻿using TestinyTestProject.Elements;
+using OpenQA.Selenium;
+
+namespace TestinyTestProject.Pages;
+
+public class DashboardPage(IWebDriver? driver, bool openByURL = false) : BasePage(driver, openByURL)
+{
+    private const string END_POINT = "DP/dashboard";
+
+    private static readonly By TitleBy = By.TagName("title");
+
+    protected override bool EvaluateLoadedStatus()
+    {
+        try
+        {
+            return TitleLabel.Text.Contains("Dashboard");
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    protected override string GetEndpoint()
+    {
+        return END_POINT;
+    }
+
+    public bool IsPageOpened()
+    {
+        return TitleLabel.Text.Contains("Dashboard");
+    }
+
+    internal UIElement TitleLabel => new(Driver, TitleBy);
+}
