047f8945785c5e9009b135c3e818a6583931e88a
Olga P. <olya-polunina@yandex.ru>
2024-04-16 20:39:05 +0300

    Added LoginPage

---
+++ TestinyTestSolution/TestinyTestProject/Pages/LoginPage.cs
@@ -0,0 +1,42 @@
+﻿using TestinyTestProject.Elements;
+using OpenQA.Selenium;
+
+namespace TestinyTestProject.Pages;
+
+public class LoginPage(IWebDriver? driver, bool openByURL = false) : BasePage(driver, openByURL)
+{
+    private const string END_POINT = "";
+
+    private static readonly By EmailInputBy = By.Name("email");
+    private static readonly By PswInputBy = By.Name("password");
+    private static readonly By LoginInButtonBy = By.XPath("//div[contains(text(),'Log in')]");
+
+    protected override string GetEndpoint()
+    {
+        return END_POINT;
+    }
+
+    protected override bool EvaluateLoadedStatus()
+    {
+        return LoginInButton.Displayed && EmailInput.Displayed;
+    }
+
+    public void SetEmail(string value)
+    {
+        EmailInput.SendKeys(value);
+    }
+
+    public void SetPassword(string value)
+    {
+        PswInput.SendKeys(value);
+    }
+
+    public void LogInClick()
+    {
+        LoginInButton.Click();
+    }
+
+    internal UIElement EmailInput => new(Driver, EmailInputBy);
+    internal UIElement PswInput => new(Driver, PswInputBy);
+    internal Button LoginInButton => new Button(Driver, LoginInButtonBy);
+}
