ecc0bf33d163bdef834104bee4ebd73a6228f798
Olga P. <olya-polunina@yandex.ru>
2024-04-16 21:05:49 +0300

    Added NavigationStep (Login method)

---
+++ TestinyTestSolution/TestinyTestProject/Steps/NavigationSteps.cs
@@ -0,0 +1,31 @@
+﻿using TestinyTestProject.Models;
+using TestinyTestProject.Pages;
+using OpenQA.Selenium;
+
+namespace TestinyTestProject.Steps;
+
+public class NavigationSteps : BaseStep
+{
+    private readonly LoginPage _loginPage;
+
+    public NavigationSteps(IWebDriver driver) : base(driver)
+    {
+        _loginPage = new LoginPage(Driver);
+    }
+    
+    public DashboardPage SuccessfulLogin(User user)
+    {
+        return Login<DashboardPage>(user);
+    }
+    
+    public T Login<T>(User user) where T : BasePage
+    {
+        LoginPage = new LoginPage(Driver);
+
+        LoginPage.SetEmail(user.Email);
+        LoginPage.SetPassword(user.Password);
+        LoginPage.LogInClick();
+
+        return (T)Activator.CreateInstance(typeof(T), Driver, false);
+    }
+}
