5904ff8cb350a34e7877405e7c560344839a15d9
Olga P. <olya-polunina@yandex.ru>
2024-04-16 16:59:57 +0300

    init sln

--- TestinyIoTestProject/TestinyIoTestProject/obj/Release/net8.0/TestinyIoTestProject.AssemblyInfo.cs
+++ TestinyIoTestProject/TestinyIoTestProject/obj/Release/net8.0/TestinyIoTestProject.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestinyIoTestProject")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+136e1a073b3c985397dee631fddaafc501d81bf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestinyIoTestProject")]
