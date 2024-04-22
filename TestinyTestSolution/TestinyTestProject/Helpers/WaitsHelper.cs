8df12a44ad36ccc7e5bd0e8da98f3a9cff0a0bfb
Olga P. <olya-polunina@yandex.ru>
2024-04-22 21:58:24 +0300

    Added FluentWaiter

--- TestinyTestSolution/TestinyTestProject/Helpers/WaitsHelper.cs
+++ TestinyTestSolution/TestinyTestProject/Helpers/WaitsHelper.cs
@@ -12,4 +12,16 @@ public class WaitsHelper(IWebDriver driver, TimeSpan timeout)
     {
         return _wait.Until(ExpectedConditions.ElementExists(locator));
     }
+
+    public IWebElement FluentWaitForElement(By locator)
+    {
+        WebDriverWait fluentWait = new WebDriverWait(driver, TimeSpan.FromSeconds(3))
+        {
+            PollingInterval = TimeSpan.FromMilliseconds(50)
+        };
+
+        fluentWait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+
+        return fluentWait.Until(_ => driver.FindElement(locator));
+    }
 }
