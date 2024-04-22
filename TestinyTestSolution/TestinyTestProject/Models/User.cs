e043665fe4bd1c49b00ab548f534497ffc001aff
Olga P. <olya-polunina@yandex.ru>
2024-04-22 20:26:56 +0300

    Удалены неиспользуемые usings

--- TestinyTestSolution/TestinyTestProject/Models/User.cs
+++ TestinyTestSolution/TestinyTestProject/Models/User.cs
@@ -4,7 +4,7 @@ namespace TestinyTestProject.Models;
 
 public record User
 {
-    public UserType UserType { get; set; };
+    public UserType UserType { get; set; }
     public string Username { get; init; } = string.Empty;
     public string Password { get; init; } = string.Empty;
     public string ApiKey { get; set; } = string.Empty;
