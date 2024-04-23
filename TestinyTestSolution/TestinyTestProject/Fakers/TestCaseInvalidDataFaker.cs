fad3b0cc1833b4a818ec1bf5ca2fefb9e794d2cb
Olga P. <olya-polunina@yandex.ru>
2024-04-23 06:09:30 +0300

    Up

--- TestinyTestSolution/TestinyTestProject/Fakers/TestCaseInvalidDataFaker.cs
+++ TestinyTestSolution/TestinyTestProject/Fakers/TestCaseInvalidDataFaker.cs
@@ -7,6 +7,6 @@ public class TestCaseInvalidDataFaker : Faker<TestCase>
 {
     public TestCaseInvalidDataFaker()
     {
-        RuleFor(b => b.Title, f => f.Random.AlphaNumeric(201));
+        RuleFor(b => b.Title, f => f.Random.AlphaNumeric(201)); 
     }
 }
